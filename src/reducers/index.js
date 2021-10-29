79e94021fc741ecc77b87966fda6403c6635f713
Yen Tran <yen.o.tran@tuni.fi>
2021-10-30 00:08:06 +0300

    update readme + fix css

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import alert from './alert';
-// import dashboard from './dashboard';
 import home from './home';
 import dashboard from './dashboard';
 export default combineReducers({ alert, home, dashboard });
